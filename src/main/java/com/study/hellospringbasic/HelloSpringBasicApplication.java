7886624dbc6c2ae0d044ac7f2d00ba49802ba4cc
ressKim <sh21dlat@gmail.com>
2021-06-07 16:20:55 +0900

    spring 기본 basic study

---
+++ src/main/java/com/study/hellospringbasic/HelloSpringBasicApplication.java
@@ -0,0 +1,13 @@
+package com.study.hellospringbasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringBasicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloSpringBasicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/study/hellospringbasic/HelloSpringBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.hellospringbasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringBasicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
