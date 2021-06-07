41b30b1b13003c4fd919678038d5f265ffd55e77
ressKim <sh21dlat@gmail.com>
2021-06-07 21:47:28 +0900

    spring web dev basic - mvc, api

--- src/main/java/com/study/hellospringbasic/controller/HelloController.java
+++ src/main/java/com/study/hellospringbasic/controller/HelloController.java
@@ -2,7 +2,7 @@ package com.study.hellospringbasic.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class HelloController {
@@ -11,5 +11,37 @@ public class HelloController {
     public String hello(Model model) {
         model.addAttribute("data", "hello!");
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@ModelAttribute("name") String name) {
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
     }
 }
